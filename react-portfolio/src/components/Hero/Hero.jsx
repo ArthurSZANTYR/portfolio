9ebab2b87ab0a3b360ac527df5a8692330973564
ArthurSZANTYR <szantyrarthur.pto@gmail.com>
2024-09-24 11:11:36 +0200

    hero section

---
+++ react-portfolio/src/components/Hero/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import styles from "./Hero.module.css"
+import { getImageUrl } from '../../utils';
+
+export const Hero = () => {
+  return(
+  <section className={styles.container}>
+    <div className={styles.content}>
+        <h1 className={styles.title}>Hi, I'm Arthur</h1>
+        <p className={styles.description}>I'm a creative technologist with interest in Tech Market and Design.
+            Reach out if you'd like to learn more!
+        </p>
+        <a href="mailto: szantyrarthur.pro@gmail.com" className={styles.contactBtn}>Contact Me</a>
+    </div>
+    <img src={getImageUrl("hero/heroImage.png")} alt="hero-image" className={styles.heroImg}/>
+    <div className={styles.topBlur} />
+    <div className={styles.bottomBlur}/>
+  </section>
+  )
+}
